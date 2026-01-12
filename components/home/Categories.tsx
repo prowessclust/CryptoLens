6b2e3a23037ff97a853747572c4354bb8906d35d
Priyanka Kumari <166229841+prowessclust@users.noreply.github.com>
2026-01-12 19:05:06 +0530

    implement categories and all coins table

---
+++ components/home/Categories.tsx
@@ -0,0 +1,73 @@
+import { fetcher } from "@/lib/coingecko.actions"
+import DataTable from "../DataTable";
+import Image from "next/image";
+import { cn, formatCurrency, formatPercentage } from "@/lib/utils";
+import { TrendingDown, TrendingUp } from "lucide-react";
+import { CategoryFallback } from "./fallback";
+
+const Categories = async () => {
+  try {
+    const categories = await fetcher<Category[]>('/coins/categories');
+    //Structure the data in columns, headers, rows
+  
+    const columns:DataTableColumn<Category>[] = [
+      { header: 'Category',
+        cellClassName: 'category-cell',
+        cell: (category) => category.name 
+      },
+      {
+        header: 'Top Gainers',
+        cellClassName: 'top-gainers-cell',
+        cell:(category) => category.top_3_coins?.map((coin) => <Image src={coin} alt={coin} key={coin} width={28} height={28} />)
+      },
+      {
+        header: '24h Change',
+        cellClassName: 'change-header-cell',
+        cell:(category) => {
+          const item = category.market_cap_change_24h;
+          const isTrendingUp = item > 0;
+          return (
+  
+            <div className={cn('change-cell', isTrendingUp? 'text-green-500' : 'text-red-500')}>
+              <p className="flex items-center">
+                {formatPercentage(item)}
+                {isTrendingUp ? (
+                  <TrendingUp width={16} height={16} />
+                ) : (
+                  <TrendingDown width={16} height={16} />
+                )
+                }
+              </p>
+            </div>
+          )
+        }
+      },
+  
+      {
+        header: 'Market Cap',
+        cellClassName: 'market-cap-cell',
+        cell:(category) => formatCurrency(category.market_cap)
+      },
+      {
+        header: '24h Volume',
+        cellClassName: 'volume-cell',
+        cell:(category) => formatCurrency(category.volume_24h)
+      }
+    ]
+  
+    return (
+      <div id="categories" className="custom-scrollbar">
+        <h4>Top Categories</h4>
+        <DataTable 
+          columns={columns}
+          data={categories?.slice(0, 10)}
+          rowKey={(_, index) => index} />
+      </div>
+    );
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+    return <CategoryFallback />
+  }
+};
+
+export default Categories
